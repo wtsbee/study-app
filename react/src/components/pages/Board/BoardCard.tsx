80ab33a9289a5d2f323a3faea096cdc878cedd16
watase.atsushi <beechan920@yahoo.co.jp>
2023-07-04 07:08:39 +0900

    FE:カードタイトルの文字が長い場合に自動で折り返す

--- react/src/components/pages/Board/BoardCard.tsx
+++ react/src/components/pages/Board/BoardCard.tsx
@@ -1,5 +1,4 @@
 import { Task } from "@/types";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 interface ChildrenProps {
@@ -12,7 +11,7 @@ const BoardCard = ({ children: task, listIndex, cardIndex }: ChildrenProps) => {
   return (
     <div className="card p-5 bg-pink-200 rounded mt-2">
       <Link to={`/board/${task.id}?list=${listIndex}&card=${cardIndex}`}>
-        {task.title}
+        <div className="break-all">{task.title}</div>
       </Link>
     </div>
   );
